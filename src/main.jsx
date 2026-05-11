6d42b287826943d7a4bcafdfba0a263e495ea658
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-10 22:24:07 -0300

    library-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql',
+  cache: new InMemoryCache(),
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
