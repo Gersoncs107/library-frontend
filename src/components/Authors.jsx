0325f80d098a3e9bb88d77c0b0f1b4b73b9ed520
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-14 19:29:41 -0300

    library-frontend-update

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,6 +1,33 @@
 import { ALL_AUTHORS } from "../queries"
 import { useQuery } from "@apollo/client/react"
 
+const SetBirthYear = (props) => {
+  return (
+    <div>
+      <h2>Set birthyear</h2>
+      <form>
+        <div>
+          name
+          <select>
+            {props.authors.map((a) => (
+              <option key={a.id} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default SetBirthYear
+
 const Authors = (props) => {
   const { loading, error, data } = useQuery(ALL_AUTHORS)
 
