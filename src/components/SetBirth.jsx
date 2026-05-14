d968374bbfb8f262f721d594344dbfac7262c05c
Gersoncs107 <gerson.silva107@gmail.com>
2026-05-14 19:30:44 -0300

    library-frontend-update

---
+++ src/components/SetBirth.jsx
@@ -0,0 +1,26 @@
+const SetBirthYear = (props) => {
+  return (
+    <div>
+      <h2>Set birthyear</h2>
+      <form>
+        <div>
+          name
+          <select>
+            {props.authors.map((a) => (
+              <option key={a.id} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input />
+        </div>
+        <button type="submit">update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default SetBirthYear
