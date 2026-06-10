a5c1677a330bbf67264570eaf2668a3ae0fcbfad
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-09 22:52:24 -0300

    library-frontend-update

--- src/queries.js
+++ src/queries.js
@@ -1,6 +1,12 @@
 import { gql } from '@apollo/client'
 
-export
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      value
+    }
+  }
+`
 
 export const ALL_AUTHORS = gql`
   query {
