8784cc56468ca01310d3b306df52795f857704fa
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-11 19:42:12 -0300

    library-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,13 @@ import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
 
 const App = () => {
+  const [token, setToken] = useState(null)
+
   const [page, setPage] = useState('authors')
+
+  if (!token) {
+    return <LoginForm setToken={setToken} />
+  }
 
   return (
     <div>
