31305f40d049a766f674901d61d687d5c37349c4
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-11 22:27:37 -0300

    library-frontend-update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { useApolloClient, useQuery } from '@apollo/client/react'
 import Authors from './components/Authors'
+import SetBirthYear from './components/SetBirthYear'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
