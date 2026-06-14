2154984bd1078beb6e9e245d643ee966b72fe92e
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-13 22:12:44 -0300

    library-frontend-update

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import { useMutation } from '@apollo/client/react'
 import { LOGIN } from '../queries'
 
-const LoginForm = ({ show, setError, setToken }) => {
+const LoginForm = ({ show, setError, setToken, setPage }) => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
   const [login] = useMutation(LOGIN, {
-    onCompleted: (data, setPage) => {
+    onCompleted: (data) => {
       const token = data.login.value
       setToken(token)
       localStorage.setItem('library-user-token', token)
+      setPage('authors')
     },
     onError: (error) => {
       setError(error.message)
