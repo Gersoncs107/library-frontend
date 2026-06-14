0153dd775c531d6c97af66b3dcd653fb424aa62a
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-13 22:31:07 -0300

    library-frontend-update

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
-import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 import { ApolloProvider } from '@apollo/client/react'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql',
+})
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : null,
+    }
+  }
+})
 
 const client = new ApolloClient({
   cache: new InMemoryCache(),
-  link: new HttpLink({
-    uri: 'http://localhost:4000/graphql',
-  }),
+  link: authLink.concat(httpLink),
 })
 
 createRoot(document.getElementById('root')).render(
