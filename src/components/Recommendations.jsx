f2b0db38a7e996a9b53cdb5cb08e1727a8651dce
Gersoncs107 <gerson.silva107@gmail.com>
2026-06-17 22:29:12 -0300

    library-frontend-update

--- src/components/Recommendations.jsx
+++ src/components/Recommendations.jsx
@@ -1,3 +1,33 @@
 import {useState, useEffect} from 'react';
 import {useQuery} from '@apollo/client/react';
 import {ALL_BOOKS} from '../queries';
+
+const Recommendations = ({show, favoriteGenre}) => {
+    const [books, setBooks] = useState([]);
+
+    const {loading, error, data} = useQuery(ALL_BOOKS);
+
+    useEffect(() => {
+        if (data) {
+            setBooks(data.allBooks);
+        }
+    }, [data]);
+
+    if (!show) {
+        return null;
+    }
+
+    return (
+        <div>
+            <h2>Recommendations</h2>
+            <p>Books in your favorite genre: {favoriteGenre}</p>
+            <ul>
+                {books
+                    .filter(book => book.genres.includes(favoriteGenre))
+                    .map(book => (
+                        <li key={book.id}>{book.title} by {book.author.name}</li>
+                    ))}
+            </ul>
+        </div>
+    );
+}
